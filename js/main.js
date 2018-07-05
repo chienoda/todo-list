69bb142a9347c7d6220d2683f3e5387842d0ed5f
Chie Noda <chienoda119@gmail.com>
2018-07-05 16:33:25 -0400

    Add function to displayTodos to show .completed

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ var todoList = {
 	todos: [],
 	displayTodos: function() {
 		if (this.todos.length === 0) {
-			console.log('Your todo list is empty.')
+			console.log('Your todo list is empty!')
 		} else {
-			console.log('Mytodos:' );
+			console.log('Mytodos:');
 			for (var i = 0; i<this.todos.length; i++) {
-			console.log( this.todos[i].todoText);
+				if (this.todos[i].completed ===true) {
+					console.log( '(x)', this.todos[i].todoText);
+				} else {
+					console.log( '( )', this.todos[i].todoText);
+				}
 		    }
 		}
 	},
