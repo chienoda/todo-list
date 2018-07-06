5a7f5ff4c1f868e321eefa6d04c8cbce9b841d18
Chie Noda <chienoda119@gmail.com>
2018-07-06 18:14:05 -0400

    Add method to displayTodos button and toggleAll button

--- js/main.js
+++ js/main.js
@@ -55,4 +55,15 @@ var todoList = {
 		this.displayTodos();
 	}
 };
+//We want to get access to the displayTodos button and toggleAll button.
+var displayTodosButton = document.getElementById('displayTodos');
+var toggleAllButton = document.getElementById('toggleAll');
+
+//We want to run displayTodos method, when user clicks the displayTodos button and toggleAll button.
+displayTodos.addEventListener('click', function () {
+	todoList.displayTodos();
+});
+toggleAll.addEventListener('click', function () {
+	todoList.toggleAll();
+});
 
