61fb6e1e9f1a56617f99c61d8af37b27965f76e5
Chie Noda <chienoda119@gmail.com>
2018-07-10 16:19:39 -0400

    Add new variable, todoTextWithCompletion

--- js/main.js
+++ js/main.js
@@ -90,12 +90,23 @@ var view = {
 	displayTodos: function () {
 		var todosUl = document.querySelector('ul');
 		todosUl.innnerHTML = '';
+
 		for (var i = 0; i < todoList.todos.length; i++) {
 			var todoLi = document.createElement('li');
-			todoLi.textContent = todoList.todos[i].todoText;
+			var todo = todoList.todos[i];
+			var todoTextWithCompletion = '';
+
+			if (todo.completed ===true) {
+				todoTextWithCompletion =  '(x)' + todo.todoText;
+				} else {
+					todoTextWithCompletion =  '( )' + todo.todoText;
+				}
+
+			todoLi.textContent = todoTextWithCompletion;
 			todosUl.appendChild(todoLi);
 		}
-	}
+	},
+
 };
 
 
